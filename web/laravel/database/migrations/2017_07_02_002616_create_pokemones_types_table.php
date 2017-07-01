39b5f2b7caefcfbc6ef22eb5b503c5040aee4661
legigante <legigante>
2017-07-02 01:39:25 +0200

    m

--- web/laravel/database/migrations/2017_07_02_002616_create_pokemones_types_table.php
+++ web/laravel/database/migrations/2017_07_02_002616_create_pokemones_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePokemonstypesTable extends Migration
+class CreatePokemonsTypesTable extends Migration
 {
 
     /**
