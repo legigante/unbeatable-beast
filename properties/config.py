882cbdbdc75e7e9c63aed48c37256eda5ed57e30
Sachka <hermesm@gmail.com>
2017-07-02 21:13:05 +0200

    Structure

---
+++ properties/config.py
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+# The config of the beast
+connector = {
+    'user': 'webUB',
+    'password': 'LeMotDePasseDeLaBete',
+    'host': '51.254.98.195',
+    'database': 'unbeatableBEAST'
+}
+
+config = {
+    'mysql': {
+        'driver': 'mysql',
+        'username': 'webUB',
+        'password': 'LeMotDePasseDeLaBete',
+        'host': '51.254.98.195',
+        'database': 'unbeatableBEAST'
+    }
+}
